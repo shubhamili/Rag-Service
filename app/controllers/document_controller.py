0489c2259cc420014247c06258739e40a8b50f4b
shubham singhk <shubhamsinghia160@gmail.com>
2026-08-16 18:17:20 +0530

    document ingestion's first stage completed

--- app/controllers/document_controller.py
+++ app/controllers/document_controller.py
@@ -1,7 +1,20 @@
 from fastapi import UploadFile,File
-
+import pymupdf
 
 async def upload_doc(file:UploadFile = File(...)):
+
+    doc = pymupdf.open(stream = file.file.read(),filetype=file.content_type)
+
+    extractedText = ""
+
+    for page in doc:
+        extractedText += page.get_text()
+
+    docExtracted = extractedText
+
+    print("____________docExtracted =================>",docExtracted)
+
     return{
-        "filename":file.filename
+        "filename":file.filename,
+        "docExtracted":docExtracted
     }
